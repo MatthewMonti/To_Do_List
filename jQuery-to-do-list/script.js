2c688cceab4968d90f728d69958101f6f9c349f4
Matthew Monti <93679020+MatthewMonti@users.noreply.github.com>
2023-05-18 23:07:41 -0400

    uncommented JS code

--- jQuery-to-do-list/script.js
+++ jQuery-to-do-list/script.js
@@ -1,42 +1,42 @@
-
-/*function newItem(){
+*/
+function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items: 
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
+//   let li = document.createElement("li");
+//   let inputValue = document.getElementById("input").value;
+ //  let text = document.createTextNode(inputValue);
+//   li.appendChild(text);
 
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
-   }
+ //  if (inputValue === '') {
+  //   alert("You must write something!");
+ //  } else {
+ //    let list = document.querySelector('#list');
+ //    list.appendChild(li);
+ //  }
 
  //2. Crossing out an item from the list of items:
-   function crossOut() {
- 		li.classList.toggle("strike");
- 	}
+  // function crossOut() {
+ 	//	li.classList.toggle("strike");
+ //	}
 
  	li.addEventListener("dblclick",crossOut);
 
  //3(i). Adding the delete button "X": 
-   let crossOutButton = document.createElement("crossOutButton");
- 	crossOutButton.appendChild(document.createTextNode("X"));
- 	li.appendChild(crossOutButton);
+   //let crossOutButton = document.createElement("crossOutButton");
+ //	crossOutButton.appendChild(document.createTextNode("X"));
+ 	//li.appendChild(crossOutButton);
 
- 	crossOutButton.addEventListener("click", deleteListItem);
+ 	//crossOutButton.addEventListener("click", deleteListItem);
  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
- 		li.classList.add("delete")
+  // function deleteListItem(){
+ 	//	li.classList.add("delete")
  	}
  // 4. Reordering the items: 
    $('#list').sortable();
 
-}
-/*
+//}
+
 
  //JAVASCRIPT WITH JQUERY 
 //  STEP 1 ADDING NEW ITEM TO LIST OF ITEMS:
